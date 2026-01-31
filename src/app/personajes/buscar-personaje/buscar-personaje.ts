99b75934192f2f5cfed63f6795f02f6bd351c673
Alfredo Carrero Muñiz <alfredoibros@gmail.com>
2026-01-31 18:29:05 +0100

    Reactivar, Baja Física y Baja Lógica v.1.0

--- src/app/personajes/buscar-personaje/buscar-personaje.ts
+++ src/app/personajes/buscar-personaje/buscar-personaje.ts
@@ -1,19 +1,23 @@
 import { ChangeDetectorRef, Component } from '@angular/core';
 import { PersonajesService } from '../../servicios/personajes-service';
-import { Router, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
+import { PersonajeService } from '../../servicios/personaje-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-buscar-personaje',
-  imports: [ButtonModule, TableModule, RouterLink],
+  imports: [ButtonModule, TableModule, RouterLink, CommonModule],
   templateUrl: './buscar-personaje.html',
   styleUrl: './buscar-personaje.css',
 })
 export class BuscarPersonaje {
   constructor(
     private personajesService: PersonajesService,
-    private route: Router,
+    private personajeService: PersonajeService,
+    private router: Router,
+    public route: ActivatedRoute,
     private cdr: ChangeDetectorRef,
   ) {}
 
@@ -22,11 +26,11 @@ export class BuscarPersonaje {
 
   editar(id: number) {
     // alert(id);
-    this.route.navigate(['/editar', id]);
+    this.router.navigate(['/editar', id]);
   }
 
   crearPersonaje(): void {
-    this.route.navigate(['/crearPersonaje']);
+    this.router.navigate(['/crearPersonaje']);
   }
 
   ngOnInit(): void {
@@ -43,6 +47,45 @@ export class BuscarPersonaje {
 
       error: (err) => {
         this.error = 'Se ha producido un error en la petición';
+      },
+    });
+  }
+
+  reactivarPersonaje(id: number) {
+    this.personajeService.reactivarPersonaje(id).subscribe({
+      next: () => {
+        alert('¡Personaje reactivado correctamente!');
+        this.router.navigate(['/personajes']);
+      },
+      error: (err) => {
+        console.error('Error al actualizar:', err);
+        this.error = 'No se pudo reactivar el personaje. Inténtalo de nuevo.';
+      },
+    });
+  }
+
+  darBajaLogica(id: number) {
+    this.personajeService.bajaLogica(id).subscribe({
+      next: () => {
+        alert('¡Personaje dado de baja temporal correctamente!');
+        this.cargarPersonajes();
+      },
+      error: (err) => {
+        console.error('Error al dar de baja lógica:', err);
+        this.error = 'No se pudo dar de baja lógica el personaje. Inténtalo de nuevo.';
+      },
+    });
+  }
+
+  darBajaFisica(id: number) {
+    this.personajeService.bajaFisica(id).subscribe({
+      next: () => {
+        alert('¡Personaje eliminado correctamente!');
+        this.cargarPersonajes();
+      },
+      error: (err) => {
+        console.error('Error al eliminar:', err);
+        this.error = 'No se pudo eliminar el personaje. Inténtalo de nuevo.';
       },
     });
   }
--- src/app/servicios/personaje-service.ts
+++ src/app/servicios/personaje-service.ts
@@ -15,11 +15,23 @@ export class PersonajeService {
     return this.http.get<any>(`${this.apiESDLA}obtenerPersonaje/${id}`);
   }
 
-  updatePersonaje(id: string, datosActualizados: any): Observable<any>{
+  updatePersonaje(id: string, datosActualizados: any): Observable<any> {
     return this.http.put<any>(`${this.apiESDLA}actualizarPersonaje/${id}`, datosActualizados);
   }
 
   createPersonaje(datos: any): Observable<any> {
     return this.http.post<any>(`${this.apiESDLA}insertarPersonaje`, datos);
+  }
+
+  bajaLogica(id: number): Observable<any> {
+    return this.http.put<any>(`${this.apiESDLA}bajaLogica/${id}`, {});
+  }
+
+  reactivarPersonaje(id: number): Observable<any> {
+    return this.http.put<any>(`${this.apiESDLA}reactivar/${id}`, {});
+  }
+
+  bajaFisica(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.apiESDLA}bajaFisica/${id}`);
   }
 }
