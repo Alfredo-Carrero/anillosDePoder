3c18ef2034f713dec5ca1195bca3217c86388865
Alfredo Carrero Muñiz <alfredoibros@gmail.com>
2026-02-23 20:08:43 +0100

    Estadísticas

--- src/app/estadisticas/estadisticas.ts
+++ src/app/estadisticas/estadisticas.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { EstadisticasService } from '../servicios/estadisticas-service';
+import { Estadisticas } from '../models/juego.model';
 
 @Component({
   selector: 'app-estadisticas',
-  imports: [],
-  templateUrl: './estadisticas.html',
-  styleUrl: './estadisticas.css',
-})
-export class EstadisticasComponent {
 
+  imports: [CommonModule, CardModule, ButtonModule],
+  templateUrl: './estadisticas.html',
+  styleUrl: './estadisticas.css'
+})
+export class EstadisticasComponent implements OnInit {
+
+  estadisticas: Estadisticas = { jugadas: 0, victorias: 0, derrotas: 0 };
+  porcentajeVictorias: number = 0;
+
+  constructor(private estadisticasService: EstadisticasService) {}
+
+  ngOnInit(): void {
+    this.cargarEstadisticas();
+  }
+
+  cargarEstadisticas() {
+    this.estadisticas = this.estadisticasService.obtenerEstadisticas();
+
+    if (this.estadisticas.jugadas > 0) {
+      this.porcentajeVictorias = Math.round((this.estadisticas.victorias / this.estadisticas.jugadas) * 100);
+    } else {
+      this.porcentajeVictorias = 0;
+    }
+  }
+
+  limpiarEstadisticas() {
+
+    localStorage.removeItem('estadisticas_esdla');
+    this.cargarEstadisticas();
+  }
 }
